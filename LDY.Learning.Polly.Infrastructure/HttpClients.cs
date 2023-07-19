31e7a4879427a9a4471405b72d825eb623e77a65
LDY <liashkod@gmail.com>
2023-07-19 20:55:16 +0200

    Add Polly infra

---
+++ LDY.Learning.Polly.Infrastructure/HttpClients.cs
@@ -0,0 +1,8 @@
+﻿namespace LDY.Learning.Polly.Infrastructure
+{
+    public static class HttpClients
+    {
+        public readonly static string BlockingHttpClient = nameof(BlockingHttpClient);
+
+    }
+}
