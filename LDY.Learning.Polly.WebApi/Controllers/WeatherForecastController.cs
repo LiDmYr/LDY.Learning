d155bf90631e240a33d006b0c8f26404aa87cd29
LDY <liashkod@gmail.com>
2023-07-19 20:55:31 +0200

    Integrated to Program and call from Controller

--- LDY.Learning.Polly.WebApi/Controllers/WeatherForecastController.cs
+++ LDY.Learning.Polly.WebApi/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using LDY.Learning.Polly.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LDY.Learning.Polly.WebApi.Controllers
@@ -6,19 +7,26 @@ namespace LDY.Learning.Polly.WebApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+        public WeatherForecastController(IHttpClientFactory httpClientFactory, ILogger<WeatherForecastController> logger) {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get() {
+        public async Task<IEnumerable<WeatherForecast>> Get() {
+            // HERE WE ARE DOING REQUEST
+            var client = _httpClientFactory.CreateClient(HttpClients.BlockingHttpClient);
+            var result = await client.GetAsync("");
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
--- LDY.Learning.Polly.WebApi/Program.cs
+++ LDY.Learning.Polly.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using LDY.Learning.Polly.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpClients();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
